eb5bc8b4f24bf5698e496535964f11b45e8ff534
Jeremy <jabulencia@yondu.com>
2024-09-05 23:42:46 +0800

    added audio and wrapper

--- scripts.js
+++ scripts.js
@@ -1,7 +1,14 @@
+let song = document.getElementById('baudio');
 $(document).ready(function() {
+    // wrapper plugin
+    $('.wrapper').wrap();
+    $('.wrapper').on("closed",function(){
+        song.play();
+    });
+
     // start
     $('.containerHeader').css({"height":$(window).height()});
-    
+
     // countdown plugin
     $("#countdown").countdownTimer({
         targetDate: new Date("2025-01-04 13:00:00"), // Wedding Day January 04 2025 1 PM
